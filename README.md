714fa8c46aa080a28f203bf6c2cafa2a0227709e
ThreeT <info@noble24.pro>
2020-01-12 01:36:44 +0300

    etc commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eComm-MPT
 Проект МПТ для портфолио
+* * *
+Результат работы можно посмотреть тут: <!-- <a href="#" target="_blank"> <img src="https://img.shields.io/badge/Site
+-Noble24.pro-orange?style=for-the-badge" alt="Страница на Noble24.pro"> </a> и --> 
+<a href="https://ecomm-mpt.netlify.com/" target="_blank"> <img src="https://img.shields.io/badge/Site-Nitlify.com-blue?style=for-the-badge" alt="Страница на Netlify.com"> </a> 
+* * *
--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	// Custom JS
+    $(document).ready(function () {
+        $('#commercial_slider-inner').slick({
+            accessibility: true,
+            autoplay: true,
+            arrows: false,
+            dots: true
+        });
+    });
 
+    $(document).ready(function () {
+        $('.commercial_board-inner').slick({
+            accessibility: true,
+            autoplay: true,
+            arrows: false,
+            // dots: true
+        });
+    });
 });
