4ee92ea5543567fde505dd1eb35eb4a265076f2e
ThreeT <info@noble24.pro>
2020-01-25 16:07:22 +0300

    Two commit

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -18,4 +18,16 @@ document.addEventListener("DOMContentLoaded", function() {
             // dots: true
         });
     });
+
+    $(document).ready(function () {
+        $('.multiple_items').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            autoplay: true,
+            slidesToScroll: 2,
+            nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+            prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        });
+    });
 });
